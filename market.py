6ea23f5c92a822413fb410d9f1c3b5f3e9724b97
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-05-16 17:31:25 +0300

    Hello world шаблон

--- market.py
+++ market.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    print(request)
+    return render_template('hello.html', context={'user': 'Vasya'})
